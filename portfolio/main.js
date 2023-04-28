bd09860e4dedf91d1129d60cd46c97e9bce81756
Tyler <tallre450@west-mec.org>
2023-04-28 14:04:32 -0700

    more work

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,19 +1,4 @@
 function boom(){
-    alert("boom!")
-    console.log("boom!")
+  console.log("boom")
+  alert("boom")
 }
-let pastPos = window.pageYOffset;
-
-window.onscroll = function() {
-  const currentScrollPos = window.pageYOffset;
-
-  if (pastPos > currentScrollPos) {
-    document.getElementById("header").classList.add("show");
-    document.getElementById("header").classList.remove("hide");
-  } else {
-    document.getElementById("header").classList.add("hide");
-    document.getElementById("header").classList.remove("show");
-  }
-
-  pastPos = currentScrollPos;
-};
