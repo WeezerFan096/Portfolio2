a3c79576616bf0d1b51dbc61fb76b9194d18b3b1
Tyler <111825114+tylerAllred@users.noreply.github.com>
2023-04-28 18:27:09 -0700

    go

--- portfolio/bio.js
+++ portfolio/bio.js
@@ -1,15 +1,15 @@
 let pastPos = window.pageYOffset;
 
-window.onscroll = function() {
-  const currentScrollPos = window.pageYOffset;
+window.mouseup = function () {
+	const currentScrollPos = window.pageYOffset;
 
-  if (pastPos > currentScrollPos) {
-    document.getElementById("header").classList.add("show");
-    document.getElementById("header").classList.remove("hide");
-  } else {
-    document.getElementById("header").classList.add("hide");
-    document.getElementById("header").classList.remove("show");
-  }
+	if (pastPos > currentScrollPos) {
+		document.getElementById("header").classList.add("show");
+		document.getElementById("header").classList.remove("hide");
+	} else {
+		document.getElementById("header").classList.add("hide");
+		document.getElementById("header").classList.remove("show");
+	}
 
-  pastPos = currentScrollPos;
+	pastPos = currentScrollPos;
 };
